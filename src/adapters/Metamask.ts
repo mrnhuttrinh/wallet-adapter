0f9b2f50a791384a341be74325f2418a0e7e1856
Trinh Nguyen <trinh.nhut@autonomous.nyc>
2022-04-18 14:50:34 +0700

    Update 2

--- src/adapters/Metamask.ts
+++ src/adapters/Metamask.ts
@@ -1,22 +1,25 @@
 import Web3 from 'web3';
+import {Contract} from 'web3-eth-contract';
+import { AbiItem } from 'web3-utils';
 import Logger from '../utils/logger';
 import { IAdapterBase, IChain, IEVMConfig } from '../types';
 import axios from 'axios';
 
 class MetamaskAdapter implements IAdapterBase {
   private name = 'Metamask';
-  private provider?: Web3;
+  private provider: Web3;
   private chainList: IChain[] = [];
   public extensionInstallUrl = 'https://metamask.io/download/';
   private config?: IEVMConfig;
   public connectedAddress?: string;
 
   constructor(_config: IEVMConfig) {
+    Logger.log(this.name, 'constructor start');
+    this.config = _config;
+    this.provider = new Web3(Object(window).ethereum);
     try {
-      Logger.log(this.name, 'constructor start');
-      this.config = _config;
-      this.provider = new Web3(Object(window).ethereum);
       this.getChainListInfo();
+      this.registerAccountChanged();
       Logger.log(this.name, 'constructor successed');
     } catch (e) {
       Logger.log(this.name, 'constructor error', e);
@@ -80,6 +83,96 @@ class MetamaskAdapter implements IAdapterBase {
       throw e;
     } finally {
       Logger.log(this.name, 'connect end');
+    }
+  }
+
+  private createContract(abi: AbiItem[], contractAddress: string): Contract {
+    try {
+      Logger.log(this.name, 'createContract start');
+      return new this.provider.eth.Contract(abi as AbiItem[], contractAddress);
+    } catch (e) {
+      Logger.log(this.name, 'createContract error', e);
+      throw e;
+    } finally {
+      Logger.log(this.name, 'createContract end');
+    }
+  }
+
+  private handleAccountChanged(accounts: string[]): void {
+    Logger.log(this.name, 'handleAccountChanged start');
+    try {
+      Logger.log(this.name, 'handleAccountChanged change', accounts);
+      this.connectedAddress = accounts[0];
+    } catch (e) {
+      Logger.log(this.name, 'handleAccountChanged error', e);
+    } finally {
+      Logger.log(this.name, 'createConthandleAccountChangedract end');
+    }
+  }
+
+  public registerAccountChanged(handler?: unknown): void {
+    if (Object(window).ethereum) {
+      if (handler) {
+        Object(window).ethereum.on('accountsChanged', handler);
+      } else {
+        Object(window).ethereum.on('accountsChanged', this.handleAccountChanged);
+      }
+    }
+  }
+
+  public removeAccountChanged(handler?: unknown): void {
+    if (Object(window).ethereum) {
+      if (handler) {
+        Object(window).ethereum.removeListener('accountsChanged', handler);
+      } else {
+        Object(window).ethereum.removeListener('accountsChanged', this.handleAccountChanged);
+      }
+    }
+  }
+
+  private handleChainChanged(chainId: string): void {
+    Logger.log(this.name, 'handleChainChanged start');
+    try {
+      Logger.log(this.name, 'handleChainChanged changed', chainId);
+    } catch (e) {
+      Logger.log(this.name, 'handleChainChanged error', e);
+    } finally {
+      Logger.log(this.name, 'handleChainChanged end');
+    }
+  }
+
+  public registerChainChanged(handler?: unknown): void {
+    if (Object(window).ethereum) {
+      if (handler) {
+        Object(window).ethereum.on('chainChanged', handler);
+      } else {
+        Object(window).ethereum.on('chainChanged', this.handleChainChanged);
+      }
+    }
+  }
+
+  public removeChainChanged(handler?: unknown): void {
+    if (Object(window).ethereum) {
+      if (handler) {
+        Object(window).ethereum.removeListener('chainChanged', handler);
+      } else {
+        Object(window).ethereum.removeListener('chainChanged', this.handleChainChanged);
+      }
+    }
+  }
+
+  private handleDisconnect(error: unknown): void {
+    Logger.log(this.name, 'handleDisconnect start');
+    Logger.log(this.name, 'handleDisconnect end');
+  }
+
+  public registerDisconnect(handler?: unknown): void {
+    if (Object(window).ethereum) {
+      if (handler) {
+        Object(window).ethereum.on('disconnect', handler);
+      } else {
+        Object(window).ethereum.on('disconnect', this.handleDisconnect);
+      }
     }
   }
 }
